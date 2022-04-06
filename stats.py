248e67ed93e8e2d3c015592ab8367afd80a27d0a
Dranikf <kobfedsur@gmail.com>
2022-04-07 02:00:21 +0300

    stats beginig - ks not ended

---
+++ stats.py
@@ -0,0 +1,9 @@
+# my common used functions make statistics in pyhton easier
+
+def KS_for_model(Y, predictions, **kwargs):
+    '''Full KS test implementation
+    in sphere of classifying model selection. 
+    KS stat, threshold and p-value'''
+    
+    
+    
