29c6349772ad2c9b3e79e6aa527791d745708270
Dranikf <kobfedsur@gmail.com>
2022-05-12 03:33:40 -0400

    data dscription fully ready

--- data_description.py
+++ data_description.py
@@ -6,7 +6,7 @@ import numpy as np
 def get_col_av_values(col):
     '''get availible values in getted column'''
     # input:
-    # col - pandas.DataFrame column wich should be used
+    # col - pandas.Series column wich should be used
     # output:
     # str line wich describe column ranges or couple of levels
     if pd.api.types.is_numeric_dtype(col):
@@ -16,3 +16,48 @@ def get_col_av_values(col):
                 np.max(col).strftime("%d.%m.%Y") + ']'
     else:
         return str(col.unique().tolist())[1:-1].replace("'", "")
+    
+    
+def get_col_obj_count(col):
+    '''
+        for categorial variables returns a couple of levels,
+        for numeric variables returns ranges
+    '''
+    # input:
+    # col - pandas.Series column wich should be used
+    # output:
+    # for numeric variable "-" and number of levels for categorial
+    
+    if pd.api.types.is_numeric_dtype(col) or\
+        pd.api.types.is_datetime64_any_dtype(col):
+        return "-"
+    else:
+        return len(col.unique())
+    
+def get_columns_desription(df):
+    '''
+        get a full discription of pandas.DataFrame
+        with Data types, ranges, Levels_number, NA_counts 
+    '''
+    # input:
+    # df - pandas.DataFrame to which you want to apply the function
+    # output
+    # pandas.DataFrame with input columns in index
+    #      and characteristics in columns
+    
+    result = pd.DataFrame(columns = [
+        'Data type', 
+        'Range',
+        'Levels number',
+        'NA count'
+    ])
+    
+    for col in df.columns:
+        result.loc[col, :] = [
+            df[col].dtype,
+            get_col_av_values(df[col]),
+            get_col_obj_count(df[col]),
+            sum(df[col].isna())
+        ]
+    
+    return result
